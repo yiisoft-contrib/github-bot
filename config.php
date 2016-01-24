1d01b8a9635362149b26363d4d4a82cb51c12d41
Carsten Brandt <mail@cebe.cc>
2016-01-24 15:22:09 +0100

    added all extension labels for moving issues

--- config.php
+++ config.php
@@ -52,8 +52,98 @@ COMMENT
 
 		[
 			'action' => 'move',
-			'label' => 'feature:redis',
+			'label' => 'ext:apidoc',
+			'repo' => 'yiisoft/yii2-apidoc',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:authclient',
+			'repo' => 'yiisoft/yii2-authclient',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:bootstrap',
+			'repo' => 'yiisoft/yii2-bootstrap',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:codeception',
+			'repo' => 'yiisoft/yii2-codeception',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:debug',
+			'repo' => 'yiisoft/yii2-debug',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:elasticsearch',
+			'repo' => 'yiisoft/yii2-elasticsearch',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:faker',
+			'repo' => 'yiisoft/yii2-faker',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:gii',
+			'repo' => 'yiisoft/yii2-gii',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:imagine',
+			'repo' => 'yiisoft/yii2-imagine',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:jui',
+			'repo' => 'yiisoft/yii2-jui',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:mongodb',
+			'repo' => 'yiisoft/yii2-mongodb',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:redis',
 			'repo' => 'yiisoft/yii2-redis',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:sphinx',
+			'repo' => 'yiisoft/yii2-sphinx',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:swiftmailer',
+			'repo' => 'yiisoft/yii2-swiftmailer',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:twig',
+			'repo' => 'yiisoft/yii2-twig',
+		],
+
+		[
+			'action' => 'move',
+			'label' => 'ext:smarty',
+			'repo' => 'yiisoft/yii2-smarty',
 		],
 
 	],
